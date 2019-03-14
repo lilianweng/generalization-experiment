TODO: gradients update in intrinsic dimension is not correct.

intrinsic_dimensions.py
@@ -32,6 +32,7 @@ class IntrinsicDimensionExperiment:
 
         self._transform_matrix_ph = tf.placeholder(tf.float64, shape=(self.D, self.d))
         self._dimension_indices_ph = tf.placeholder(tf.int32, shape=(self.d,))
+        self._dimension_mask_ph = tf.placeholder(tf.int32, shape=(self.D,))
 
     def _initialize(self):
         self.sess.run(self.iterator.initializer)
@@ -42,9 +43,13 @@ class IntrinsicDimensionExperiment:
         return self.sess.run([self.loss_ph, self.accuracy_ph], feed_dict=feed_dict)
 
     def sample_transform_matrix(self):
-        """Matrix P in the paper of size (seld.D, d
+        """Matrix P in the paper of size (D, d)
+        Columns of P are normalized to unit length.
         """
-        return np.random.random((self.D, self.d))
+        matrix = np.random.random((self.D, self.d))
+        for i in range(self.d):
+            matrix[:, i] /= np.sum(matrix[:, i])
+        return matrix
 
     def sample_selected_dimensions(self):
         indices = np.random.choice(range(self.D), size=self.d, replace=False)
@@ -55,6 +60,9 @@ class IntrinsicDimensionExperiment:
         return self.sess.run(tf_flat_vars(tf.trainable_variables()))
 
     def gradient_update(self, lr):
+        """
+        TODO: Change it.
+        """
         var_list = tf.trainable_variables()
 
         optimizer = tf.train.AdamOptimizer(lr)
@@ -103,11 +111,12 @@ class IntrinsicDimensionExperiment:
         for epoch in range(n_epoches):
             while True:
                 try:
-                    self.sess.run(train_op, feed_dict=feed_dict)
+                    _, loss = self.sess.run([train_op, self.loss_ph], feed_dict=feed_dict)
+                    logging.info(f"[step:{step}] loss={loss}")
                     step += 1
                     if step % 100 == 0:
                         eval_loss, eval_acc = self._get_eval_results(feed_dict)
-                        logging.info(f"epoch:{epoch} step:{step} eval_loss:{eval_loss} "
+                        logging.info(f"[epoch:{epoch}|step:{step}] eval_loss:{eval_loss} "
                                      f"eval_acc:{eval_acc}")
                 except tf.errors.OutOfRangeError:
                     self.sess.run(self.iterator.initializer)
@@ -119,6 +128,6 @@ class IntrinsicDimensionExperiment:
 
 
 if __name__ == '__main__':
-    d = 5000
-    exp = IntrinsicDimensionExperiment(d, [512])
-    exp.train()
+    d = 50
+    exp = IntrinsicDimensionExperiment(d, [1024])
+    exp.train(lr=0.01)

layer_equality.py
@@ -11,6 +11,7 @@ configure_logging()
 class LayerEqualityExperiment:
 
     def __init__(self, layer_sizes):
+        self.output_filename = "data/layer_equality_results.json"
         self.sess = make_session()
 
         dataset, (_, _), (self.x_test, self.y_test) = prepare_mnist_dataset()
@@ -29,7 +30,8 @@ class LayerEqualityExperiment:
         np.random.seed(int(time.time()))
 
     def _initialize(self, lr):
-        train_op = tf.train.MomentumOptimizer(lr, momentum=0.9).minimize(self.loss_ph)
+        # train_op = tf.train.MomentumOptimizer(lr, momentum=0.9).minimize(self.loss_ph)
+        train_op = tf.train.AdamOptimizer(lr).minimize(self.loss_ph)
 
         self.sess.run(self.iterator.initializer)
         self.sess.run(tf.global_variables_initializer())
@@ -112,17 +114,18 @@ class LayerEqualityExperiment:
                 except tf.errors.OutOfRangeError:
                     eval_accuracy = self._get_eval_accuracy()
                     logging.info(
-                        f">>> epoch:{epoch} step:{step} train_loss:{train_loss} eval_accuracy:{eval_accuracy} lr:{lr}")
+                        f">>> epoch:{epoch} step:{step} train_loss:{train_loss:.4f} eval_accuracy:{eval_accuracy:.4f} lr:{lr}")
                     self.sess.run(self.iterator.initializer)
                     break
 
-            lr *= 0.8
-            # results += self.measure_layer_robustness(eval_accuracy, epoch)
-            ## save to disk in every loop
-            # with open('layer_equality_results.json', 'w') as fout:
-            #    json.dump(results, fout)
+            if epoch > 0 and epoch % 10 == 0:
+                lr *= 0.5
+                results += self.measure_layer_robustness(eval_accuracy, epoch)
+                # save to disk in every loop
+                with open(self.output_filename, 'w') as fout:
+                    json.dump(results, fout)
 
 
 if __name__ == '__main__':
     exp = LayerEqualityExperiment([256, 256])
-    exp.train(100, 0.01)
+    exp.train(100, 0.002)